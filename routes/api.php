acb053548db338477c453ed22331e161b32cf94b
Stan <stan.mikhnevich@gmail.com>
2024-06-13 11:12:03 +0300

    Added book api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ApiAuthorController;
+use App\Http\Controllers\Api\ApiBookController;
 use App\Http\Controllers\Api\Auth\ApiLoginController;
 use App\Http\Controllers\Api\Auth\ApiLogoutController;
 use Illuminate\Http\Request;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('authors', ApiAuthorController::class);
+    Route::apiResource('books', ApiBookController::class);
 
     Route::prefix('auth')->group(function () {
         Route::get('/user', function (Request $request) {
