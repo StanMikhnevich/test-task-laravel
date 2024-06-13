a0650e67022230200555e1a75fda4d4e896ff0c6
Stan <stan.mikhnevich@gmail.com>
2024-06-13 11:13:24 +0300

    Added book config

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -31,7 +31,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::ACCOUNT;
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -45,23 +45,24 @@ class VerificationController extends Controller
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 
-	/**
-	 * Mark the authenticated user's email address as verified.
-	 *
-	 * @param EmailVerificationRequest $request
-	 * @return RedirectResponse
-	 */
-	public function __invoke(EmailVerificationRequest $request): RedirectResponse
-	{
-		if ($request->user()->hasVerifiedEmail()) {
-			return redirect()->intended(RouteServiceProvider::ACCOUNT)
-				->withSuccess(__('user.verificationAlreadyDone'));
-		}
+    /**
+     * Mark the authenticated user's email address as verified.
+     *
+     * @param EmailVerificationRequest $request
+     * @return RedirectResponse
+     */
+    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(RouteServiceProvider::HOME)
+                ->withSuccess(__('user.verificationAlreadyDone'));
+        }
 
-		if ($request->user()->markEmailAsVerified()) {
-			event(new Verified($request->user()));
-		}
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
 
-		return redirect()->intended(RouteServiceProvider::ACCOUNT)
-			->withSuccess(__('user.verificationDone'));
-	}}
+        return redirect()->intended(RouteServiceProvider::HOME)
+            ->withSuccess(__('user.verificationDone'));
+    }
+}
---
+++ config/book.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'image' => [
+        'sizeX' => 1440,
+        'sizeY' => 2560,
+    ],
+];
